9c4fd61545fc3b46ce20497b41c16285f548f3b9
apocolyp4 <dcarr12@caledonian.ac.uk>
2014-01-24 22:47:20 +0000

    added asswhip project
    aye

--- cdtsgj2014/cdtsgj2014/Game1.cs
+++ cdtsgj2014/cdtsgj2014/Game1.cs
@@ -18,6 +18,12 @@ namespace cdtsgj2014
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Model myModel;
+        float aspectRatio = 1.0f;
+        Vector3 cameraPosition = new Vector3(0.0f, 50.0f, 400.0f);
+        Vector3 modelPosition = Vector3.Zero;
+        float modelRotation = 0.0f;
+
 
         public Game1()
             : base()
@@ -47,6 +53,8 @@ namespace cdtsgj2014
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            myModel = Content.Load<Model>("WoodenCabinFbx");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -84,6 +92,31 @@ namespace cdtsgj2014
             GraphicsDevice.Clear(Color.DarkOliveGreen);
 
             // TODO: Add your drawing code here
+
+            Matrix[] transforms = new Matrix[myModel.Bones.Count];
+            myModel.CopyAbsoluteBoneTransformsTo(transforms);
+
+            // Draw the model. A model can have multiple meshes, so loop.
+            foreach (ModelMesh mesh in myModel.Meshes)
+            {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] *
+                        Matrix.CreateRotationY(modelRotation)
+                        * Matrix.CreateTranslation(modelPosition);
+                    effect.View = Matrix.CreateLookAt(cameraPosition,
+                        Vector3.Zero, Vector3.Up);
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(
+                        MathHelper.ToRadians(45.0f), aspectRatio,
+                        1.0f, 10000.0f);
+                }
+                // Draw the mesh, using the effects set above.
+                mesh.Draw();
+            }
+
 
             base.Draw(gameTime);
         }
